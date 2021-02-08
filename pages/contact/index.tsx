5d794d18b5bf3bc147d33a672143b062827aaca7
20minhajj <minhajjharith@gmail.com>
2021-02-09 01:14:36 +0300

    setting the map library

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,6 +1,29 @@
-import Head from 'next/head';
-import NavBar from '../../components/navBar';
-import Footer from '../../components/footer';
+import Head from "next/head";
+import { GoogleMap, LoadScript,Marker } from "@react-google-maps/api";
+import NavBar from "../../components/navBar";
+import Footer from "../../components/footer";
+import styles from "../../styles/Contact.module.scss";
+
+const API_KEYS = "AIzaSyBe_pZIcnKoz9Lxknnj2Fm09yLm6At-7RM";
+
+const center = {
+  lat: -6.815475,
+  lng: 36.682995,
+};
+const containerStyle = {
+  width: "100vh",
+  height: "400px",
+};
+const position = {
+  lat: -3.3706968,
+  lng: 36.6850069,
+}
+
+// 3.3706968,36.6850069,
+
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
 
 export default function Contact() {
   return (
@@ -9,20 +32,31 @@ export default function Contact() {
         <meta name="description" content="Get in touch with us." />
         <title>CFX-contact us</title>
       </Head>
-      <main>
-        <NavBar />
-        <section>
-          <h2>We Would Love To Hear From You.</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            fugiat repellat voluptatem a distinctio non voluptate, alias
-            repudiandae!
-          </p>
+      <NavBar />
+      <main className={styles.container}>
+        <section className={styles.articles}>
+          <LoadScript googleMapsApiKey={API_KEYS}>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={17}
+            >
+              {/* Child components, such as markers, info windows, etc. */}
+              <>
+                <Marker onLoad={onLoad} position={position} />
+              </>
+            </GoogleMap>
+          </LoadScript>
         </section>
         <section>
-          <form>
-            <input type="text" name="name" id="name" />
-            <input type="email" name="email" id="email" />
+          <form className={styles.form_container}>
+            <input type="text" name="name" id="name" placeholder="John Doe" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="johndoe@xyz.com"
+            />
             <textarea
               name="message"
               id="message"
